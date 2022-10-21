e792b85b0a766642a9d2d88e8973a9727bcbe342
UpaniAyanga <115032236+UpaniAyanga@users.noreply.github.com>
2022-10-21 21:11:28 +0530

    Update prime numbers.py

--- prime numbers.py
+++ prime numbers.py
@@ -1,7 +1,4 @@
-# Complete the code to find if a given number is a prime number?
-# The program will take a positive integer greater than 1 as input and indicate if it is a prime number by saying "prime",
-# and if it is not a prime number saying "not a prime". 
-# Note there are 3 places in the given code that you need to fix for this code to work properly and give the expected output. 
+# a code created to find whether any given integer is a prime number or not  
 
 i = int(input('enter number'))
 j = 2
